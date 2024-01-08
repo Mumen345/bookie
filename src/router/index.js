1fe981af2ee49130028b2837869528925cc753f7
mumen345 <kingxosas@gmail.com>
2024-01-08 09:45:17 +0100

    added the 404 screen

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import NotFound from '../views/NotFound.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,12 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: "*",
+    // path: "/:catchAll(.*)",
+    name: "NotFound",
+    component: NotFound,
   },
   // {
   //   path: '/about',
---
+++ src/views/NotFound.vue
@@ -0,0 +1,35 @@
+<template>
+    <section class=" section_container">
+        <h2>
+            OOPS
+        </h2>
+        <p>404 Error</p>
+        <p>The page you're looking for cannot be found</p>
+        <img src="../assets/images/404_img.png" alt="">
+        <router-link class="secondary_button" to="/">Back to Home</router-link>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.section_container {
+    background-color: #E7E7E7;
+    background-image: url("../assets/images/404_bg.svg");
+    background-size: cover;
+    background-position: center;
+    background-attachment: fixed;
+    background-repeat: no-repeat;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+}
+</style>
